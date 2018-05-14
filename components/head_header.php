209767e4fc0d9cf73dd6e21f717d20e83e1ceccd
Ariel <Arielgp.70@gmail.com>
2018-05-14 01:47:55 -0400

    first iteration

---
+++ components/head_header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>ISE & E</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://webthemez.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet"> 
+<link href="css/flexslider.css" rel="stylesheet" /> 
+<link href="css/style.css" rel="stylesheet" />
+<!-- Vendor Styles -->
+<link href="css/magnific-popup.css" rel="stylesheet"> 
+<!-- Block Styles -->
+<link href="css/gallery-1.css" rel="stylesheet">
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+</head>
+<body>
+<div id="wrapper" class="home-page">
+	<div class="topbar">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12"> 	  
+					<p class="pull-left hidden-xs"><i class="fa fa-clock-o"></i><span>Mon - Sat 8.00 - 18.00. Sunday CLOSED</span></p>
+					<p class="pull-right"><i class="fa fa-phone"></i>Tel No. (+001) 123-456-789</p>
+				</div>
+			</div>
+		</div>
+	</div><!-- fin: topbar -->
+	<!-- start header -->
+	<header>
+		<div class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/></a>
+				</div><!-- fin: navbar-header -->
+				<div class="navbar-collapse collapse ">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.php">Iinicio</a></li> 
+						<li class="dropdown">
+							<a href="#" data-toggle="dropdown" class="dropdown-toggle">Quienes somos<b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="about.php">Company</a></li>
+								<li><a href="about.php">Our Team</a></li>
+								<li><a href="about.php">News</a></li> 
+								<li><a href="about.php">Investors</a></li>
+							</ul>
+						</li><!-- fin: dropdown --> 
+						<li><a href="services.php">Servicios</a></li>
+							<li><a href="portfolio.php">Portfolio</a></li>
+							<li><a href="pricing.php">Pricing</a></li>
+							<li><a href="contact.php">Contactatenos</a></li>
+					</ul><!-- fin: nav navbar-nav -->
+				</div><!-- fin: navbar-collapse collapse -->
+				</div>
+			</div><!-- container -->
+		</div><!-- fin: navbar navbar-default navbar-static-top -->
+	</header>
