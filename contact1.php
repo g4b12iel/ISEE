9baf093e2b757d4abf17ea320bb2258447e43fb8
g4b12iel <39676985+g4b12iel@users.noreply.github.com>
2018-07-31 00:22:50 -0400

    Rename contact.php to contact1.php

---
+++ contact1.php
@@ -0,0 +1,97 @@
+<?php
+	include("components/head_header.php");
+?>
+<!-- end header -->
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="pageTitle">Contactenos</h2>
+			</div>
+		</div>
+	</div>
+	</section>
+	<section id="content">
+	
+	<div class="container">
+		<div class="row"> 
+							<div class="col-md-6">
+								<div class="about-logo">
+									<h5>GERENCIA GENERAL</h5>
+									<u>ING. GONZALO BERNAL IPORRE</u><br><br>
+									 <img src="img/service1.jpg" alt="logo" width=150px height=150px border-radius= 75px/></a></style><br><br><br>
+									<h5>GERENCIA COMERCIAL NACIONAL</h5>
+									<u href="#" data-placement="top">JACKELINNE  GARNICA</u>
+									<h5>GERENCIA ADMINISTRATIVA FINANCIERA</h5>
+									<u>LIC . LINETH RIOS</u>
+									<h5>DEP. TECNICO NACIONAL</h5>
+									<u>ARIEL MARTINEZ C.</u>
+								</div>           	
+								 
+							</div>
+							<div class="col-md-6">
+								<div class="about-logo">
+									<h5>Informes </h5>
+   <p class="Estilo1"><strong>Oficina Central&nbsp; Cochabamba </strong></p>
+   <p>Depto T&eacute;cnico: Pantaleon Dalence y Am&eacute;rica   #1722<br />
+     Telefono piloto&nbsp; (591) 4   4451195 </p>
+   
+    <p class="Estilo1"> <strong>Regional La Paz</strong></p>
+    <p >Rosario Bernal</p>
+    <p>Av. Jaime Freyre Nº 2944<br/>
+   Tel. (591) 2 - 2426305 Cel. 70584441-65150694  </p>
+    <p><a href="mailto:rbernal@iseye.com.bo">rbernal@iseye.com.bo</a></p>
+    <p class="Estilo1"><strong> Regional Santa Cruz</strong></p>
+    <p > Edwin Chavez</p>
+    <p>Calle Camargo N-597</p>
+   <p>Tel. (591) 3 - 3532539  Cel. 70401969</p>
+    <p><a href="mailto:echavez@iseye.com.bo">echavez@iseye.com.bo</a></p>
+    <p class="Estilo1"><strong> Regional Oruro</strong></p>
+    <p >Roberto Patiño C.</p>
+    <p>C/Sucre 430 entre Tejerina y Tarapaca</p>
+   <p>Tel. (591) 2 - 5287448 Cel. 70412848</p>
+   <p><a href="mailto:rpatino@iseye.com.bo">rpatino@iseye.com.bo</a></p>
+   <p class="Estilo1">  <strong>Regional Sucre</strong></p>
+   <p>Huberth Rodriguez</p>
+   <p>Calle Camargo Nº 597</p>
+    <p> Tel. (591) 4 - 6425647 Cel. 70313857 </p>
+   <p><a href="mailto: hrodriguez@iseye.com.bo" >hrodriguez@iseye.com.bo</a></p>
+   <p class="Estilo1"> <strong>Regional Potosi</strong></p>
+   <p>Jesus Campos Nogales</p>
+   <p>C/ Chayanta 1052 entre Ingavi y Serrudo<br/>
+    Tel. (591) 2 -6221535-6225225 Cel. 77536716</p>
+   <p><a href="mailto:jcampos@iseye.com.bo">jcampos@iseye.com.bo</a> </p>
+   <p class="Estilo1"> <strong>Regional Tarija</strong></p>
+   <p>Esteban Vasquez</p>
+   <p>c/ Abaroa entre Mendez y Suipacha N. 194<br/>
+     Tel.(591) 4 - 6610671 Cel. 70215169-72994234 </p>
+   <p><a href="mailto:evasquez@iseye.com.bo">evasquez@iseye.com.bo</a> </p>
+      <p class="Estilo1"><strong> Regional Beni</strong></p>
+   <p>Dario Suarez Ando</p>
+   <p>C/ Martin Pascual No 137 <br/>
+     Tel.(591) 3 - 4629481 Cel. 70268385 </p>
+   <p> <a href="mailto:dsuarez@iseye.com.bo">dsuarez@iseye.com.bo</a>&nbsp; </p>
+      <p class="Estilo1"><strong> Regional Pando</strong></p>
+   <p>Nelson Quispe Flores</p>
+   <p>Av. Fernández Molina S/n<br/>
+     Tel.(591) 3 - 8424720 Cel. 60180850</p>
+   <p> <a href="mailto:nquispe@iseye.com.bo">nquispe@iseye.com.bo</a>&nbsp; </p>
+   <p>&nbsp;</p>
+								</div>           	
+								 
+							</div>
+							
+						</div>
+
+	<div class="row">
+								
+								<div class="col-md-6">
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script><div style="overflow:hidden;height:500px;width:600px;"><div id="gmap_canvas" style="height:500px;width:600px;"></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style><a class="google-map-code" href="http://www.trivoo.net" id="get-map-data">trivoo</a></div><script type="text/javascript"> function init_map(){var myOptions = {zoom:14,center:new google.maps.LatLng(40.805478,-73.96522499999998),mapTypeId: google.maps.MapTypeId.ROADMAP};map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);marker = new google.maps.Marker({map: map,position: new google.maps.LatLng(40.805478, -73.96522499999998)});infowindow = new google.maps.InfoWindow({content:"<b>The Breslin</b><br/>2880 Broadway<br/> New York" });google.maps.event.addListener(marker, "click", function(){infowindow.open(map,marker);});infowindow.open(map,marker);}google.maps.event.addDomListener(window, 'load', init_map);</script>
+								</div>
+							</div>
+	</div>
+ 
+  </section>
+<?php
+	include("components/javascript_footer.php");
+?>
